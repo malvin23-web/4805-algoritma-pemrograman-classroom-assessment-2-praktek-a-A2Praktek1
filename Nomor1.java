50dd37c79bfcc79106a5dd9a2729ad2d1aa17c8a
malvin <malvinjordy@gmail.com>
2024-12-12 08:52:09 +0700

    Update Nomor1.java

--- Nomor1.java
+++ Nomor1.java
@@ -1,13 +1,25 @@
 import java.util.Scanner;
 
 public class Nomor1 {
+    public static void tampilkanBilanganGanjil(int n) {
+        System.out.println("Bilangan ganjil dari 0 hingga " + n + " adalah:");
+        for (int i = 0; i <= n; i++) {
+            if (i % 2 != 0) {
+                System.out.print(i + " ");
+            }
+        }
+        System.out.println(); 
+    }
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        int N = input.nextInt();
-        /* Pekerjaan anda mulai dari sini */
-
-        /* Pekerjaan anda berakhir sini */
+        
+        System.out.print("Masukkan bilangan n: ");
+        int n = input.nextInt();
+        
+        tampilkanBilanganGanjil(n);
+        
+        input.close();
 
         System.out.println(Nomor1.class.getDeclaredMethods().length); 
     }
